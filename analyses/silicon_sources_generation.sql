196486e6c9fc660431ea0ede471e6a200acdb5ee
Hesham <hesham.h.96@gmail.com>
2023-10-24 19:14:35 +0300

    Initial Commit

---
+++ analyses/silicon_sources_generation.sql
@@ -0,0 +1,19 @@
+{{ codegen.generate_source(schema_name='price_system'
+,generate_columns=True
+,include_data_types=False
+,include_descriptions=True
+,include_database=True
+,include_schema=True
+,name='oracle_price_system'
+) }}
+
+
+
+
+
+
+
+
+
+
+
